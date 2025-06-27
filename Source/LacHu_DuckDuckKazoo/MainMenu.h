dc42f0b0dc827ffb7c92d68c988cde762cb094c4
Irongo <hugolach@hotmail.fr>
2025-06-27 11:28:55 +0200

    GreatSuccess

---
+++ Source/LacHu_DuckDuckKazoo/MainMenu.h
@@ -0,0 +1,65 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MyGameInstance.h"
+#include "MainMenu.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class LACHU_DUCKDUCKKAZOO_API UMainMenu : public UUserWidget
+{
+    GENERATED_BODY()
+
+protected:
+    virtual bool Initialize();
+    virtual void NativeDestruct() override;
+
+private:
+    UPROPERTY(meta = (BindWidget))
+    class UButton* HostButton;
+
+    UPROPERTY(meta = (BindWidget))
+    class UButton* JoinButton;
+
+    UPROPERTY(meta = (BindWidget))
+    class UWidgetSwitcher* Screens;
+
+    UPROPERTY(meta = (BindWidget))
+    class UWidget* Menu;
+
+    UPROPERTY(meta = (BindWidget))
+    class UWidget* JoinScreen;
+
+    UPROPERTY(meta = (BindWidget))
+    class UEditableTextBox* InputIP;
+
+    UPROPERTY(meta = (BindWidget))
+    class UButton* JoinConfirm;
+
+    UPROPERTY(meta = (BindWidget))
+    class UButton* CancelButton;
+
+    UFUNCTION()
+    void HostGame();
+
+    UFUNCTION()
+    void JoinGame();
+
+    UFUNCTION()
+    void OpenJoinScreen();
+
+    UFUNCTION()
+    void CancelJoin();
+
+    UMyGameInstance* MenuGameInstance;
+
+public:
+    void SetMenuGameInstance(UMyGameInstance* GameInstance);
+
+    void Setup();
+};
---
+++ Source/LacHu_DuckDuckKazoo/MyGameInstance.h
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "MyGameInstance.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class LACHU_DUCKDUCKKAZOO_API UMyGameInstance : public UGameInstance
+{
+    GENERATED_BODY()
+
+public:
+    UMyGameInstance();
+
+    virtual void Init() override;
+
+    UFUNCTION(Exec)
+    void Host();
+
+    UFUNCTION(Exec)
+    void Join(const FString& IPAddress);
+
+    UFUNCTION(Exec, BlueprintCallable, Category = "Menu")
+    void MainMenu();
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "UI")
+    TSubclassOf<UUserWidget> WidgetClass;
+
+
+private:
+    UPROPERTY()
+    class UMainMenu* MainMenuWidget;
+};
